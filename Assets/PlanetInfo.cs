c1bc9c967b13e1e9c1afd8ac9cb04394546690c3
won2001 <djdjvlf2001@naver.com>
2024-10-11 12:55:18 +0900

    [Feat] 행성정보 UI틀 구현
    행성을 터치하면 정보UI가 나오게 해주는 기능 구현

---
+++ Assets/PlanetInfo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetInfo : MonoBehaviour
+{
+    [SerializeField] GameObject infoPanel;
+    [SerializeField] Text infoText;
+
+    private void Update()
+    {
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Planet"))
+                {
+                    infoPanel.SetActive(true); // 정보 창 활성화
+                    infoText.text = hit.transform.name + "의 정보"; // 행성 정보 업데이트
+                }
+            }
+        }
+    }
+}
